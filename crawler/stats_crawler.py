a541ee99bcc6e91ebec679f61cbe46725c036a0a
Yudong An <ayd@immiq.top>
2023-01-31 19:33:45 +0800

    [add] synergy data [fix] quit browser

--- crawler/stats_crawler.py
+++ crawler/stats_crawler.py
@@ -21,42 +21,72 @@ for key, value in data.items():
 lanes = ['top', 'jungle', 'middle', 'bottom', 'support']
 
 
+class Data:
+    def __init__(self, champion, vslane, win_rate, games):
+        self.champion = champion
+        self.vslane = vslane
+        self.win_rate = win_rate
+        self.games = games
+
+    def __eq__(self, other):
+        return self.champion == other.champion and self.vslane == other.vslane
+
+    def __hash__(self):
+        return hash((self.champion, self.vslane))
+
+
 @retry
 def crawler(champion_name, lane):
     url = "https://lolalytics.com/lol/" + champion_name + \
         "/build/?lane=" + lane + "&tier=all"
-    driver = webdriver.Firefox()
-    driver.get(url)
+
+    try:
+        driver = webdriver.Firefox()
+        driver.get(url)
+        driver.execute_script("arguments[0].scrollIntoView();", driver.find_element_by_class_name(
+            "CounterButtons_set__99iaF"))
+
+        sorted_datas = get_data(driver)
+
+        with open(champion_name + "_" + lane + "_against.csv", "w") as csvfile:
+            writer = csv.writer(csvfile)
+            writer.writerow(["champion", "vslane", "win_rate", "games"])
+            for data in sorted_datas:
+                writer.writerow([data.champion, data.vslane,
+                                data.win_rate, data.games])
+
+        driver.find_element_by_xpath(
+            "//*[@id=\"root\"]/div[4]/div[11]/div[3]/div[2]/div[2]/div[1]").click()
+
+        sorted_datas = get_data(driver)
+
+        with open(champion_name + "_" + lane + "_synergy.csv", "w") as csvfile:
+            writer = csv.writer(csvfile)
+            writer.writerow(["champion", "lane", "win_rate", "games"])
+            for data in sorted_datas:
+                writer.writerow([data.champion, data.vslane,
+                                data.win_rate, data.games])
+
+        driver.quit()
+    except:
+        driver.quit()
+
+
+def get_data(driver):
     counters = driver.find_elements_by_class_name(
         "CountersPanel_counters__U8zc5")
-
-    driver.execute_script(
-        "arguments[0].scrollIntoView();", counters[0])
-
-    class Data:
-        def __init__(self, champion, vslane, win_rate, games):
-            self.champion = champion
-            self.vslane = vslane
-            self.win_rate = win_rate
-            self.games = games
-
-        def __eq__(self, other):
-            return self.champion == other.champion and self.vslane == other.vslane
-
-        def __hash__(self):
-            return hash((self.champion, self.vslane))
 
     datas = []
 
     for i in range(1, len(counters) + 1):
         counter = counters[i - 1]
-        Panel_data__dtE8F = counter.find_element_by_class_name(
+        panels = counter.find_element_by_class_name(
             "Panel_data__dtE8F")
         for j in range(0, 16):
             driver.execute_script(
                 "document.getElementsByClassName(\"Panel_data__dtE8F\")[" + str(i) + "].scroll(" + str(j * 890) + ", 0)")
 
-            Cell_cell__383UV = Panel_data__dtE8F.find_elements_by_class_name(
+            Cell_cell__383UV = panels.find_elements_by_class_name(
                 "Cell_cell__383UV")
             for cell in Cell_cell__383UV:
                 a = cell.find_element_by_tag_name("a")
@@ -66,8 +96,13 @@ def crawler(champion_name, lane):
 
                 match = re.search(r'/vs/(.*?)/', parsed_url.path)
 
-                champion = match.group(1)
-                vslane = query.get("vslane", [None])[0]
+                if (match == None):
+                    match = re.search(r'/lol/(.*?)/build', parsed_url.path)
+                    champion = match.group(1)
+                    vslane = query.get("lane", [None])[0]
+                else:
+                    champion = match.group(1)
+                    vslane = query.get("vslane", [None])[0]
 
                 numbers = [float(n) if ',' not in n else float(
                     n.replace(',', '')) for n in cell.text.split('\n')]
@@ -78,17 +113,11 @@ def crawler(champion_name, lane):
                 data = Data(champion, vslane, win_rate, games)
                 datas.append(data)
 
-    driver.quit()
-
     unique_datas = list(set(datas))
     sorted_datas = sorted(
         unique_datas, key=lambda x: x.games, reverse=True)
-    with open(champion_name + "_" + lane + ".csv", "w") as csvfile:
-        writer = csv.writer(csvfile)
-        writer.writerow(["champion", "vslane", "win_rate", "games"])
-        for data in sorted_datas:
-            writer.writerow([data.champion, data.vslane,
-                            data.win_rate, data.games])
+
+    return sorted_datas
 
 
 for lane in lanes:
